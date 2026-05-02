0af03155e43113719eb4f2d374b97fb15002936c
Clayton Wheeler <cswheeler@gmail.com>
2026-05-01 22:50:44 -0500

    chore(protospy): allow building without tokio_unstable

--- src/tokio_util.rs
+++ src/tokio_util.rs
@@ -2,7 +2,7 @@ use color_eyre::Result;
 use tokio::task::{AbortHandle, JoinHandle, JoinSet};
 use tracing::{Instrument, error};
 
-#[allow(dead_code)]
+#[cfg(tokio_unstable)]
 pub fn spawn_instrumented<Fut, T>(name: &str, future: Fut) -> Result<JoinHandle<Result<T>>>
 where
     Fut: Future + Send + 'static,
@@ -15,6 +15,20 @@ where
     )?)
 }
 
+#[cfg(not(tokio_unstable))]
+pub fn spawn_instrumented<Fut, T>(name: &str, future: Fut) -> Result<JoinHandle<Result<T>>>
+where
+    Fut: Future + Send + 'static,
+    Fut::Output: Into<Result<T>> + Send + Sync,
+    T: Send + Sync + 'static,
+{
+    let err_name = name.to_owned();
+    Ok(tokio::task::spawn(
+        async move { log_err(err_name, future.await.into()) }.instrument(tracing::Span::current()),
+    ))
+}
+
+#[cfg(tokio_unstable)]
 pub fn spawn_instrumented_on<Fut, T>(
     join_set: &mut JoinSet<Result<T>>,
     name: &str,
@@ -29,6 +43,23 @@ where
     Ok(join_set.build_task().name(name).spawn(
         async move { log_err(err_name, future.await.into()) }.instrument(tracing::Span::current()),
     )?)
+}
+
+#[cfg(not(tokio_unstable))]
+pub fn spawn_instrumented_on<Fut, T>(
+    join_set: &mut JoinSet<Result<T>>,
+    name: &str,
+    future: Fut,
+) -> Result<AbortHandle>
+where
+    Fut: Future + Send + 'static,
+    Fut::Output: Into<Result<T>> + Send + Sync,
+    T: Send + Sync + 'static,
+{
+    let err_name = name.to_owned();
+    Ok(join_set.spawn(
+        async move { log_err(err_name, future.await.into()) }.instrument(tracing::Span::current()),
+    ))
 }
 
 pub fn log_err<T>(name: String, res: Result<T>) -> Result<T> {
