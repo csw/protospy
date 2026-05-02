5147040b9d64552580c853598db0fa947b7db56e
Clayton Wheeler <cswheeler@gmail.com>
2026-05-01 19:57:49 -0500

    feat(protospy): embed and serve UI content

--- src/server/router.rs
+++ src/server/router.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use axum::{Router, routing::get};
 
 use crate::server::App;
+use crate::server::ui;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -12,11 +13,11 @@ pub struct AppState {
 pub fn router(app: Arc<App>) -> Router {
     let state = AppState { app };
     Router::new()
-        .route("/", get(|| async { "Hello, world!" }))
         .route("/info", get(crate::server::info::get_info))
         .route(
             "/service/{name}/events",
             get(crate::server::events::handle_events),
         )
+        .fallback(ui::static_handler)
         .with_state(state)
 }
