fedc23b276650802576b12b00ff19f2e9f9ccc82
Clayton Wheeler <cswheeler@gmail.com>
2026-05-05 22:01:42 -0500

    feat(protospy): add minimal TLS client config

--- src/proxy/client.rs
+++ src/proxy/client.rs
@@ -1,19 +1,41 @@
 use std::time::Duration;
 
-use hyper_util::rt::{TokioExecutor, TokioTimer};
+use hyper_rustls::{ConfigBuilderExt, HttpsConnector};
+use hyper_util::{
+    client::legacy::connect::HttpConnector,
+    rt::{TokioExecutor, TokioTimer},
+};
 
 use super::body;
 
 pub type Client = hyper_util::client::legacy::Client<
-    hyper_util::client::legacy::connect::HttpConnector,
+    hyper_rustls::HttpsConnector<hyper_util::client::legacy::connect::HttpConnector>,
     ClientBody,
 >;
 
 pub type ClientBody = body::upstream::RequestBody;
 
-pub fn build() -> Client {
+pub fn tls_conn() -> HttpsConnector<HttpConnector> {
+    let _ = rustls::crypto::ring::default_provider().install_default();
+
+    let tls = rustls::ClientConfig::builder()
+        .with_webpki_roots()
+        .with_no_client_auth();
+
+    hyper_rustls::HttpsConnectorBuilder::new()
+        .with_tls_config(tls)
+        .https_or_http()
+        .enable_http1()
+        .build()
+}
+
+pub fn build_tls() -> Client {
     hyper_util::client::legacy::Client::builder(TokioExecutor::new())
         .pool_timer(TokioTimer::new())
         .pool_idle_timeout(Duration::from_secs(30))
-        .build_http::<ClientBody>()
+        .build(tls_conn())
+}
+
+pub fn build() -> Client {
+    build_tls()
 }
