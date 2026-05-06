1214f5834f09786e2e8c9fb3f4cb2d897cb44102
Clayton Wheeler <cswheeler@gmail.com>
2026-05-06 16:32:43 -0500

    fix(protospy): report mapped IPv4 addresses in native form

--- src/proxy/headers.rs
+++ src/proxy/headers.rs
@@ -1,4 +1,4 @@
-use std::sync::LazyLock;
+use std::{net::IpAddr, sync::LazyLock};
 
 use color_eyre::{Result, eyre::eyre};
 use http::{HeaderMap, HeaderName, Request, Response};
@@ -60,7 +60,7 @@ pub fn request_headers<T>(target: &str, req: &Request<T>, conn: &ConnInfo) -> Re
     }
     req_h.append(
         X_FORWARDED_FOR,
-        conn.client.ip().to_string().parse().unwrap(),
+        format_ip_addr(conn.client.ip()).parse().unwrap(),
     );
     req_h.append(X_FORWARDED_PROTO, conn.protocol.parse()?);
 
@@ -80,6 +80,19 @@ pub fn response_headers<B>(orig: &Response<B>) -> Result<HeaderMap> {
     );
 
     Ok(headers)
+}
+
+fn format_ip_addr(addr: IpAddr) -> String {
+    match addr {
+        IpAddr::V4(addr4) => addr4.to_string(),
+        IpAddr::V6(addr6) => {
+            if let Some(addr4) = addr6.to_ipv4_mapped() {
+                addr4.to_string()
+            } else {
+                addr6.to_string()
+            }
+        }
+    }
 }
 
 /// Generates a Via header value, e.g. '1.1 protospy'
@@ -115,106 +128,147 @@ mod tests {
     use http::request::Builder;
     use http_body_util::Empty;
     use hyper::body::Bytes;
+    use rstest::rstest;
 
     use super::*;
 
     const CLIENT_IP: &str = "127.0.0.1";
     const CLIENT: &str = "127.0.0.1:45678";
+    const CLIENT_V6_MAPPED: &str = "[::ffff:127.0.0.1]:45678";
     const TARGET: &str = "localhost:80";
 
     // ==== Request tests ====
+    mod request {
+        use super::*;
 
-    #[test]
-    fn test_x_forwarded_for_added() {
-        let h = build_mapped_req(|b| b);
-        assert_eq!(header_val(&h, "X-Forwarded-For"), Some(CLIENT_IP))
-    }
+        #[test]
+        fn test_x_forwarded_for_added() {
+            let h = build_mapped_req(|b| b);
+            assert_eq!(header_val(&h, "X-Forwarded-For"), Some(CLIENT_IP))
+        }
 
-    #[test]
-    fn test_x_forwarded_for_appended() {
-        let orig = "192.168.1.1";
-        let h = build_mapped_req(|b| b.header("x-forwarded-for", orig));
-        assert_eq!(header_vals(&h, "X-Forwarded-For"), vec!(orig, CLIENT_IP))
-    }
+        #[test]
+        fn test_x_forwarded_for_appended() {
+            let orig = "192.168.1.1";
+            let h = build_mapped_req(|b| b.header("x-forwarded-for", orig));
+            assert_eq!(header_vals(&h, "X-Forwarded-For"), vec!(orig, CLIENT_IP))
+        }
 
-    #[test]
-    fn test_x_forwarded_proto_added() {
-        let h = build_mapped_req(|b| b);
-        assert_eq!(header_val(&h, "X-Forwarded-Proto"), Some("http"));
-    }
+        #[test]
+        fn test_x_forwarded_for_v4_mapped() {
+            let req = Builder::new().body(empty()).unwrap();
+            let h = request_headers(
+                TARGET,
+                &req,
+                &ConnInfo {
+                    protocol: "http".to_string(),
+                    client: CLIENT_V6_MAPPED.parse().unwrap(),
+                },
+            )
+            .unwrap();
 
-    #[test]
-    fn test_x_forwarded_host_added() {
-        let orig = "localhost:3000";
-        let h = build_mapped_req(|b| b.header(hyper::header::HOST, orig));
-        assert_eq!(header_val(&h, "X-Forwarded-Host"), Some(orig));
-    }
+            assert_eq!(header_val(&h, "X-Forwarded-For"), Some(CLIENT_IP))
+        }
 
-    #[test]
-    fn test_x_forwarded_host_appended() {
-        let orig = "localhost:3000";
-        let orig_fwd = "altair:80";
-        let h = build_mapped_req(|b| b.header("Host", orig).header("X-Forwarded-Host", orig_fwd));
-        assert_eq!(header_vals(&h, "X-Forwarded-Host"), vec!(orig_fwd, orig))
-    }
+        #[test]
+        fn test_x_forwarded_proto_added() {
+            let h = build_mapped_req(|b| b);
+            assert_eq!(header_val(&h, "X-Forwarded-Proto"), Some("http"));
+        }
 
-    #[test]
-    fn test_keep_alive_stripped() {
-        let h = build_mapped_req(|b| b.header(KEEP_ALIVE, "timeout=5"));
-        assert!(!h.contains_key(KEEP_ALIVE));
-    }
+        #[test]
+        fn test_x_forwarded_host_added() {
+            let orig = "localhost:3000";
+            let h = build_mapped_req(|b| b.header(hyper::header::HOST, orig));
+            assert_eq!(header_val(&h, "X-Forwarded-Host"), Some(orig));
+        }
 
-    #[test]
-    fn test_request_via() {
-        let h = build_mapped_req(|b| b);
-        assert_eq!(header_val(&h, "Via"), Some("1.1 protospy"));
-    }
+        #[test]
+        fn test_x_forwarded_host_appended() {
+            let orig = "localhost:3000";
+            let orig_fwd = "altair:80";
+            let h =
+                build_mapped_req(|b| b.header("Host", orig).header("X-Forwarded-Host", orig_fwd));
+            assert_eq!(header_vals(&h, "X-Forwarded-Host"), vec!(orig_fwd, orig))
+        }
 
-    #[test]
-    fn test_host() {
-        let h = build_mapped_req(|b| b.header("Host", "localhost:3000"));
-        assert_eq!(header_val(&h, "host"), Some(TARGET))
+        #[test]
+        fn test_keep_alive_stripped() {
+            let h = build_mapped_req(|b| b.header(KEEP_ALIVE, "timeout=5"));
+            assert!(!h.contains_key(KEEP_ALIVE));
+        }
+
+        #[test]
+        fn test_request_via() {
+            let h = build_mapped_req(|b| b);
+            assert_eq!(header_val(&h, "Via"), Some("1.1 protospy"));
+        }
+
+        #[test]
+        fn test_host() {
+            let h = build_mapped_req(|b| b.header("Host", "localhost:3000"));
+            assert_eq!(header_val(&h, "host"), Some(TARGET))
+        }
     }
 
     // ==== Response tests ====
 
-    #[test]
-    fn test_res_strip_connection() {
-        let h =
-            basic_response_headers(&[("connection", "keep-alive"), ("keep-alive", "timeout=5")]);
-        assert!(!h.contains_key("connection"));
-        assert!(!h.contains_key("keep-alive"));
+    mod response {
+        use super::*;
+
+        #[test]
+        fn test_res_strip_connection() {
+            let h = basic_response_headers(&[
+                ("connection", "keep-alive"),
+                ("keep-alive", "timeout=5"),
+            ]);
+            assert!(!h.contains_key("connection"));
+            assert!(!h.contains_key("keep-alive"));
+        }
+
+        #[test]
+        fn test_res_strip_proxy_authenticate() {
+            let h = basic_response_headers(&[
+                ("connection", "keep-alive"),
+                ("keep-alive", "timeout=5"),
+                (
+                    "proxy-authenticate",
+                    "Basic realm=\"Dev\", charset=\"UTF-8\"",
+                ),
+            ]);
+            assert!(!h.contains_key("proxy-authenticate"));
+        }
+
+        #[test]
+        fn test_res_strip_lone_keep_alive() {
+            let h = basic_response_headers(&[("keep-alive", "timeout=5")]);
+            assert!(!h.contains_key("keep-alive"));
+        }
+
+        #[test]
+        fn test_res_via() {
+            let h = response_headers(
+                &Response::builder()
+                    .version(http::Version::HTTP_11)
+                    .body(())
+                    .unwrap(),
+            )
+            .unwrap();
+            assert_eq!(header_val(&h, "Via"), Some("1.1 protospy"));
+        }
     }
 
-    #[test]
-    fn test_res_strip_proxy_authenticate() {
-        let h = basic_response_headers(&[
-            ("connection", "keep-alive"),
-            ("keep-alive", "timeout=5"),
-            (
-                "proxy-authenticate",
-                "Basic realm=\"Dev\", charset=\"UTF-8\"",
-            ),
-        ]);
-        assert!(!h.contains_key("proxy-authenticate"));
-    }
+    mod formatting {
+        use super::*;
 
-    #[test]
-    fn test_res_strip_lone_keep_alive() {
-        let h = basic_response_headers(&[("keep-alive", "timeout=5")]);
-        assert!(!h.contains_key("keep-alive"));
-    }
-
-    #[test]
-    fn test_res_via() {
-        let h = response_headers(
-            &Response::builder()
-                .version(http::Version::HTTP_11)
-                .body(())
-                .unwrap(),
-        )
-        .unwrap();
-        assert_eq!(header_val(&h, "Via"), Some("1.1 protospy"));
+        #[rstest]
+        #[case("127.0.0.1", "127.0.0.1")]
+        #[case("::1", "::1")]
+        #[case("::ffff:127.0.0.1", "127.0.0.1")]
+        fn test_format_ip_addr(#[case] addr: IpAddr, #[case] expected: &str) {
+            let formatted = format_ip_addr(addr);
+            assert_eq!(formatted, expected);
+        }
     }
 
     fn make_headers(literals: &[(&str, &str)]) -> HeaderMap {
